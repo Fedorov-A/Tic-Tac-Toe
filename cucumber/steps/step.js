9e3014365c62d594dd06fe53c9eab968a3876def
Fedorov Alexey <fedorov2505@yandex.com>
2020-03-18 11:20:54 +0300

    Add feature and step definition files

---
+++ cucumber/steps/step.js
@@ -0,0 +1,11 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('поле', (dataTable) => {
+  'pending';
+});
+When('игрок ходит в клетку', (dataTable) => {
+  'pending';
+});
+Then('поле становится', (dataTable) => {
+  'pending';
+});
