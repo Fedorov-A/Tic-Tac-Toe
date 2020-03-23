cbc59771037fe172157c77d0bc450f19847ca222
Alexey <fedorov-alexey-vladimirovich@yandex.com>
2020-03-24 00:42:55 +0300

    Add CORS to server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const routes = require('./routes.js');
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
