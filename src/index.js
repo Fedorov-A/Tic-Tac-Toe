895582c95675d3399a950317a9405215dca7a753
Alexey <fedorov-alexey-vladimirovich@yandex.com>
2020-04-21 00:10:34 +0300

    Add web socket

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./server');
+const http = require('./server');
 const logger = require('./lib/logger');
 
 const port = 2000;
 
-app.listen(port, () => logger.log(`Example app listening on port ${port}!`));
+http.listen(port, () => logger.log(`Example app listening on port ${port}!`));
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const routes = require('./routes');
 const users = require('./lib/users');
 
@@ -10,4 +13,11 @@ app.use(cors());
 app.use(users.authentication);
 app.use(routes);
 
-module.exports = app;
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+module.exports = http;
